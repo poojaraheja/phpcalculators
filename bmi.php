2f183cc1023c1ecca3b4e84ef5000d8889bcb2bd
corbinbot <viveklawaniya98@gmail.com>
2023-05-12 18:07:39 +0530

    c

--- bmi.php
+++ bmi.php
@@ -130,78 +130,13 @@ if (isset($_POST['submit'])) {
             </div>
 
         </div>
-        <footer id="Footer" style="background-color:#fff;" class="page-footer font-small stylish-color-dark pt-4 mt-4">
-            <div style="background-color:#fff;" class="container">
-                <div class="row">
-                    <div class="col-md-4 mx-auto">
-                        <!-- Content -->
-                        <h5 class=" text-uppercase font-weight-bold mt-3 mb-4">About our Company</h5>
+        <?php
+        include_once "footer.php";
 
-                    </div>
-
-                    <div id="link10" class="about col-md-4  mx-auto mt-3 mb-4 text-center">
-                        <h6 class="text-uppercase font-weight-bold  mb-4 t" style="color: #70df70;
-    font-weight: bold;
-    font-size: 25px;">About Us</h6>
-
-                        <p>
-                            Welcome to
-                            <a href=" https://uhealthy.in/">uhealthy,</a>
-                        </p>
-                        <p>
-                            where health-minded individuals come together to support
-                        </p>
-                        <p>
-                            each other on their journeys to better health.
-                        </p>
-
-                    </div>
-
-
-                    <div class="col-md-4 mx-auto mt-3 mb-4 text-lg-end text-md-end text-sm-center text-center">
-                        <h6 class="text-uppercase font-weight-bold mb-4 " style="color: #70df70;
-    font-weight: bold;
-    font-size: 25px;">Contact Us</h6>
-                        <p>
-                            <i class="fas fa-home mr-3"></i> +918010373181
-                        </p>
-                        <p>
-                            <i class="fas fa-envelope mr-3"></i> Info@uhealthy.in
-                        </p>
-                        <p>
-                            2, Gaura Complex,
-                            <br>
-                            Near Matrix Hospital,
-                            <br>
-                            Haldwani – 263139
-                            <br>
-                            Uttarakhand, India
-                        </p>
-
-                    </div>
-                </div>
-            </div>
-
-
-
-            <!-- Copyright -->
-            <div style="background-color:#fff;" class="footer-copyright text-center py-3">
-                <p>
-                    <a href="">Our Services</a>
-                </p>
-                <p>
-                    <a href="">Privacy Policy</a>
-                </p>
-            </div>
-            <div style="background-color:#fff;" class="footer-copyright text-center py-3">
-                Copyright© 2023 | <a href=" https://uhealthy.in/">Uhealthy.in</a><br>
-                Design and Develop by <a href="https://www.magcloudsolutions.in/">Mag Cloud Solutions Pvt. Ltd.</a>
-            </div>
-
-        </footer>
+        ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
-            </script>
+        </script>
     </body>
 
 </html>
---
+++ footer.php
@@ -0,0 +1,69 @@
+<footer id="Footer" style="background-color:#fff;" class="page-footer font-small stylish-color-dark pt-2 mt-4">
+    <div style="background-color:#fff;" class="container">
+        <div class="row">
+            <div class="col-md-4 mx-auto">
+                <!-- Content -->
+                <h5 class=" text-uppercase font-weight-bold mt-3 mb-4">About our Company</h5>
+
+            </div>
+
+            <div id="link10" class="about col-md-4  mx-auto mt-3 mb-4 text-center">
+                <h6 class="text-uppercase font-weight-bold  mb-4 t" style="color: #70df70;
+    font-weight: bold;
+    font-size: 25px;">About Us</h6>
+
+                <p>
+                    Welcome to
+                    <a href=" https://uhealthy.in/">uhealthy,</a>
+                </p>
+                <p>
+                    where health-minded individuals come together to support
+                </p>
+                <p>
+                    each other on their journeys to better health.
+                </p>
+
+            </div>
+
+
+            <div class="col-md-4 mx-auto mt-3 mb-0 text-lg-end text-md-end text-sm-center text-center">
+                <h6 class="text-uppercase font-weight-bold mb-4 " style="color: #70df70;
+    font-weight: bold;
+    font-size: 25px;">Contact Us</h6>
+                <p>
+                    <i class="fas fa-home mr-3"></i> +918010373181
+                </p>
+                <p>
+                    <i class="fas fa-envelope mr-3"></i> Info@uhealthy.in
+                </p>
+                <p>
+                    2, Gaura Complex,
+                    <br>
+                    Near Matrix Hospital,
+                    <br>
+                    Haldwani - 263139
+                    <br>
+                    Uttarakhand, India
+                </p>
+
+            </div>
+        </div>
+    </div>
+
+
+
+    <!-- Copyright -->
+    <div style="background-color:#fff;" class="footer-copyright text-center py-2">
+        <p>
+            <a href="">Our Services</a>
+        </p>
+        <p>
+            <a href="">Privacy Policy</a>
+        </p>
+    </div>
+    <div style="background-color:#fff;" class="footer-copyright text-center py-2">
+        Copyright© 2023 | <a href=" https://uhealthy.in/">Uhealthy.in</a><br>
+        Design and Developed by | <a href="https://www.magcloudsolutions.in/">Mag Cloud Solutions Pvt. Ltd.</a>
+    </div>
+
+</footer>
