e374385b8dfa3b42174f5c3aad4e9c39bcb451a2
poojaraheja <rahejapooja670@gmal.com>
2023-05-15 12:10:13 +0530

    code

---
+++ fat.php
@@ -0,0 +1,149 @@
+<?php
+
+require "function.php";
+
+if (isset($_POST['submit'])) {
+    $response = fat_cal($_POST['name'], $_POST['mobile'], $_POST['waist'], $_POST['wrist'], $_POST['hip'], $_POST['forearm'], $_POST['gender'], $_POST['interested'], $_POST['weight']);
+}
+
+?>
+<!doctype html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bootstrap demo</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <?php
+        include_once "navbar.php";
+
+        ?>
+        <div class="container">
+
+            <div class="form-wrap">
+                <form id="survey-form" action="bmi.php" method="post">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label id="name-label" for="name">Name</label>
+                                <input type="text" name="name" id="name" placeholder="Enter your name"
+                                    class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label id="email-label" for="email">Mobile</label>
+                                <input type="number" name="mobile" placeholder="Enter your number" class="form-control"
+                                    required>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label id="number-label" for="number">Weight</label>
+                                <input type="number" name="weight" id="number" min="10" max="99" class="form-control"
+                                    placeholder="Enter your weight(Kg)">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label id="email-label" for="email">Waist</label>
+                                <input type="number" name="waist" id="email" placeholder="Enter your waist in inches"
+                                    class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label id="email-label" for="email">Wrist Measurement(At fullest point)</label>
+                                <input type="number" name="wrist" id="email"
+                                    placeholder="Wrist Measurement(At Fullest Point)" class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label id="email-label" for="email">Hip Measurement(At fullest point)</label>
+                                <input type="number" name="hip" id="email"
+                                    placeholder="Hip Measurement(At Fullest Point)" class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label id="email-label" for="email">Forearm Measurement(At fullest point)</label>
+                                <input type="number" name="forearm" id="email"
+                                    placeholder="Forearm Measurement(At Fullest Point)" class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Gender</label>
+                                <select id="dropdown" name="gender" class="form-control" required>
+
+                                    <option value="male">Male</option>
+                                    <option value="female">Female</option>
+
+
+                                </select>
+
+
+                            </div>
+
+
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Interested in knowing More</label>
+                                <select id="dropdown" name="interested" class="form-control" required>
+
+                                    <option value="yes">Yes</option>
+                                    <option value="no">No</option>
+
+
+                                </select>
+
+
+                            </div>
+
+
+                        </div>
+
+
+
+
+                    </div>
+
+
+
+
+
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <button type="submit" id="submit" class="btn btn-primary btn-block"
+                                name="submit">Submit</button>
+                        </div>
+                    </div>
+
+                </form>
+                <?php echo @$response ?>
+            </div>
+
+        </div>
+        <?php
+        include_once "footer.php";
+
+        ?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+            </script>
+    </body>
+
+</html>
--- function.php
+++ function.php
@@ -71,4 +71,109 @@ function bmi_cal($name, $mobile, $email, $weight, $height, $inches)
 
 
 }
+
+function fat_cal($name, $mobile, $waist, $wrist, $weight, $hip, $forearm, $gender, $interested)
+{
+    if ($gender == 'women') {
+        $body_weight = (intval($weight * 0.732)) + 9.987;
+        $wrist_measure = intval($wrist) / 3.140;
+        $waist_measure = intval($waist) * 0.157;
+        $hip_measure = intval($hip) * 0.249;
+        $forearm_measure = intval($forearm) * 0.434;
+        $lean_body_mass_factor = $body_weight + $wrist_measure - $waist_measure - $hip_measure + $forearm_measure;
+        $body_fat_weight = $weight - $lean_body_mass_factor;
+        $body_fat_per = ($body_fat_weight * 100) / $weight;
+        if ($body_fat_per > 10 && $body_fat_per < 12) {
+            $condition = "Essential Fat";
+        } elseif ($body_fat_per > 14 && $body_fat_per < 20) {
+            $condition = "Athletes";
+
+        } elseif ($body_fat_per > 21 && $body_fat_per < 24) {
+            $condition = "Fitness";
+
+        } elseif ($body_fat_per > 25 && $body_fat_per < 31) {
+            $condition = "Acceptable";
+
+        } elseif ($body_fat_per > 32) {
+            $condition = "Obese";
+        }
+
+
+    } elseif ($gender == 'male') {
+        $men_weight = ($weight * 1.082) + 94.42;
+        $men_waist = $waist * 4.15;
+        $men_lean_body_mass_factor = $men_weight - $men_waist;
+        $men_body_fat_weight = $weight = $men_lean_body_mass_factor;
+        $men_body_fat_percentage = ($men_body_fat_weight * 100) / $weight;
+        if ($men_body_fat_percentage > 2 && $men_body_fat_percentage < 4) {
+            $condition = "Essential Fat";
+        } elseif ($men_body_fat_percentage > 6 && $men_body_fat_percentage < 13) {
+            $condition = "Athletes";
+
+        } elseif ($men_body_fat_percentage > 14 && $men_body_fat_percentage < 17) {
+            $condition = "Fitness";
+
+        } elseif ($men_body_fat_percentage > 18 && $men_body_fat_percentage < 25) {
+            $condition = "Acceptable";
+
+        } elseif ($men_body_fat_percentage > 25) {
+            $condition = "Obese";
+        }
+
+
+
+
+
+    }
+    if ($gender == 'women') {
+
+        $message = "Hii " . $name . ", your BMI is: " . $body_fat_per . ". You are " . $condition . "!";
+
+        $ch = curl_init();
+        // $url = 'https://fullstackmtech.com/api/send.php';
+        $url = "https://fullstackmtech.com/api/send.php?number=91" . $mobile . "&type=text&message=" . urlencode($message) . "&instance_id=644FF20D6602A&access_token=e5eb155cecb6a89011cb8568e1135663";
+
+        // $data = array(
+        //     'number'       => '91' . $mobile,
+        //     'type'         => 'text',
+        //     'message'      => $message,
+        //     'instance_id'  => '644FF20D6602A',
+        //     'access_token' => 'e5eb155cecb6a89011cb8568e1135663'
+
+        // );
+
+
+
+
+
+
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+
+        curl_setopt($ch, CURLOPT_POST, 1);
+
+        // curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+
+
+
+        $response = curl_exec($ch);
+
+
+
+
+        curl_close($ch);
+
+
+
+
+        return $response;
+
+    } elseif ($gender == ' men') {
+
+    }
+}
+
 ?>
